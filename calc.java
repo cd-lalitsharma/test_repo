6ef26bf62ed8c52132f8de0ac86bc3883936f6d3
cd-lalitsharma <lalit.sharma@tothenew.com>
2019-02-11 16:16:42 +0530

    added calc.java

---
+++ calc.java
@@ -0,0 +1,18 @@
+class Calc{
+
+	public static void main(String args[]){
+	
+		Calc a = new Calc();
+
+		//add function
+		a.add();
+
+		//substract functino
+		a.sub();
+	}
+
+	 public void add(){
+	 System.out.println(10+12);
+	 }
+
+}
